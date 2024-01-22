fd336d35cbb800dad5e7d65019cb4d7c357be08c
Linus Lach <linus.lach@gmail.com>
2024-01-22 13:01:41 +0100

    Added State_Dicts and compiled every model with same params

--- utils.py
+++ utils.py
@@ -31,7 +31,7 @@ def plot_tensor_images(
     image_tensor, num_images=25, size=(1, 28, 28), save_fig=False, epoch=0
 ):
     image_unflat = image_tensor.detach().cpu().view(-1, *size)
-    image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    image_grid = make_grid(image_unflat[:num_images], nrow=8)
     plt.axis("off")
     plt.imshow(image_grid.permute(1, 2, 0).squeeze())
     if save_fig:
